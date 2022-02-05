81fdcd721b30e1440a5cb2267a88ff0a4517efb6
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-02-05 12:30:52 -0600

    pull and push incrementer

--- public/index.js
+++ public/index.js
@@ -1,18 +1,36 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let myURLJSON = await fetch('http://localhost:9001/counter')
+    let newJSON = await myURLJSON.json()
+    console.log(newJSON)
+
+    let countValue = newJSON.value;
+
+    function serverUpdate(countValue) {
+        fetch('http://127.0.0.1:9001/counter', {
+            method: 'PATCH', 
+            headers: {
+                'Content-Type': 'application/json'
+            }, 
+            body: JSON.stringify({
+                "value": countValue,
+            })
+        })
+    }
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        serverUpdate(countValue)
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        serverUpdate(countValue)
     }
 
     incrementButton.addEventListener('click', increment);
